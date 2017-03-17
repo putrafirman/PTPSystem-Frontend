1e9d2d387334a97428ca870a65d952a121285a85
Putra Firman Ardiansyah <ardidroid97@gmail.com>
2017-03-17 12:34:38 +0700

    add html code to header and footer

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import VersionRelease from "./footer/VersionRelease";
+import Copyright from "./footer/Copyright";
+
 export default class Header extends React.Component {
 
 
@@ -10,6 +13,16 @@ export default class Header extends React.Component {
     return(
       <div>
         #ini footer
+        <VersionRelease/>
+        <Copyright/>
+        <footer class="main-footer">
+        <div class="container">
+          <div class="pull-right hidden-xs">
+            <b>Version</b> 1.0.0
+          </div>
+          <strong>Copyright &copy; 2017. Malang Dev Team.</strong> All rights reserved.
+        </div>
+        </footer>
       </div>
     )
   }
--- src/js/component/Header.js
+++ src/js/component/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Nav from "./header/Nav";
+import UserProfile from "./header/UserProfile";
 
 export default class Header extends React.Component {
 
@@ -12,7 +13,20 @@ export default class Header extends React.Component {
     return(
       <div>
         <Nav/>
+        <UserProfile/>
         #ini header
+        <header class="main-header">
+        <nav class="navbar navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <a href="#" class="navbar-brand"><b>ETR</b> System</a>
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                <i class="fa fa-bars"></i>
+              </button>
+            </div>
+          </div>
+        </nav>
+        </header>
       </div>
     )
   }
