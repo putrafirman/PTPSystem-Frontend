86e4bd8df089421dd55d7c89b93bb6b43c9c49d0
Putra Firman Ardiansyah <ardidroid97@gmail.com>
2017-03-17 12:33:08 +0700

    add template for footer

---
+++ src/js/component/footer/Copyright.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class Copyright extends React.Component {
+
+
+  //main pada js
+  render() {
+
+    return(
+      <div>
+        #ini copyright
+      </div>
+    )
+  }
+}
---
+++ src/js/component/footer/VersionRelease.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class VersionRelease extends React.Component {
+
+
+  //main pada js
+  render() {
+
+    return(
+      <div>
+        #ini version
+      </div>
+    )
+  }
+}
